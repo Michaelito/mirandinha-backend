772952aeaf6695acf4a609914e235ddf63ad19f7
Michael Regis dos Santos <michael.santos@deal.com.br>
2024-04-25 15:33:10 -0300

    ajustes clientes

--- app/controllers/clientes.controller.js
+++ app/controllers/clientes.controller.js
@@ -4,56 +4,64 @@ const Op = db.Sequelize.Op;
 
 
 exports.findAll = async (req, res) => {
-    const nome = req.query.nome;
-    var condition = nome ? {
+  const nome = req.query.nome;
+  var condition = nome
+    ? {
         nome: {
-            [Op.like]: `%${nome}%`
-        }
-    } : null;
-    const pageAsNumber = Number.parseInt(req.query.page);
-    const sizeAsNumber = Number.parseInt(req.query.size);
+          [Op.like]: `%${nome}%`,
+        },
+      }
+    : null;
+  const pageAsNumber = Number.parseInt(req.query.page);
+  const sizeAsNumber = Number.parseInt(req.query.size);
 
-    let page = 0;
-    if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
-        page = pageAsNumber;
-    }
+  let page = 0;
+  if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
+    page = pageAsNumber;
+  }
 
-    let size = 20;
-    if (
-      !Number.isNaN(sizeAsNumber) &&
-      !(sizeAsNumber > 50) &&
-      !(sizeAsNumber < 1)
-    ) {
-      size = sizeAsNumber;
-    }
-    const clientWithCount = await Clientes.findAndCountAll({
-      where: condition,
-      limit: size,
-      offset: page * size,
-    });
-    res.send({
-      status: true,
-      message: "The request has succeeded",
-      limit: size,
-      page: page,
-      totalPages: Math.ceil(clientWithCount.count / Number.parseInt(size)),
-      data: {
-        products: clientWithCount.rows,
-      },
-    });
-}
+  let size = 20;
+  if (
+    !Number.isNaN(sizeAsNumber) &&
+    !(sizeAsNumber > 50) &&
+    !(sizeAsNumber < 1)
+  ) {
+    size = sizeAsNumber;
+  }
+  const clientWithCount = await Clientes.findAndCountAll({
+    where: condition,
+    limit: size,
+    offset: page * size,
+  });
+  res.send({
+    status: true,
+    message: "The request has succeeded",
+    limit: size,
+    page: page,
+    totalPages: Math.ceil(clientWithCount.count / Number.parseInt(size)),
+    data: {
+      clientes: clientWithCount.rows,
+    },
+  });
+};
 
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Clientes.findByPk(id)
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error retrieving Data with id=" + id
-            });
-        });
+  Clientes.findByPk(id)
+    .then((data) => {
+      res.send({
+        status: true,
+        message: "The request has succeeded",
+        data: {
+          cliente: data,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error retrieving Data with id=" + id,
+      });
+    });
 };
