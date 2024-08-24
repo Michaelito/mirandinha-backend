32c2adc7982ad8f7940a83aeac93037bb46d8993
Michaelito <michaelito.regis@gmail.com>
2024-08-24 17:44:32 -0300

    Add dashboard

--- app/controllers/dashboard.controller.js
+++ app/controllers/dashboard.controller.js
@@ -1,29 +1,56 @@
 const sequelize = require("../config/database");
 const db = require("../models");
 const Pedidos = db.pedidos;
-const Op = db.Sequelize.Op;
+const { fn, col, Op } = require("sequelize");
 
 // Retrieve all GrupoFormats from the database.
 exports.findAll = async (req, res) => {
-  const id = req.params.id;
+  const id = req.body.id;
 
-  const resultPedidos = await Pedidos.findAll({
-    where: { id: id },
-  });
+  try {
+    // Consulta para somar quantidade e soma total por mês e ano
+    const resultPedidos = await Pedidos.findAll({
+      attributes: [
+        [fn("DATE_FORMAT", col("createdAt"), "%Y"), "year"],
+        [fn("DATE_FORMAT", col("createdAt"), "%m"), "month"],
+        [fn("COUNT", col("id")), "count_orders"],
+        [fn("SUM", col("total_geral")), "sum_total_orders"],
+      ],
+      where: {
+        user_id: id, // Condição específica (opcional)
+      },
+      group: [
+        "user_id",
+        fn("DATE_FORMAT", col("createdAt"), "%Y"),
+        fn("DATE_FORMAT", col("createdAt"), "%m"),
+      ], // Agrupa por ano e mês
+      order: [[fn("DATE_FORMAT", col("createdAt"), "%Y-%m"), "ASC"]],
+    });
 
-  const results = await sequelize.query(
-    "SELECT * FROM cores WHERE status = :status",
-    {
-      replacements: { status: 1 },
-      type: sequelize.QueryTypes.SELECT,
+    // Verifica se a contagem de pedidos é zero
+    if (resultPedidos.length === 0) {
+      res.status(200).send({
+        status: true,
+        message: "Data not found"
+      });
     }
-  );
 
-  res.status(200).send({
-    status: true,
-    message: "The request has succeeded",
-    data: {
-      Grupos: results,
-    },
-  });
+    // Enviando resposta com os dados de pedidos
+    res.status(200).send({
+      status: true,
+      message: "The request has succeeded",
+      data: {
+        dashboard: resultPedidos,
+      },
+    });
+  } catch (error) {
+    console.error("Erro ao buscar dados dos pedidos:", error);
+
+    // Enviando resposta de erro
+    res.status(500).send({
+      status: false,
+      message: "Ocorreu um erro ao buscar os dados dos pedidos",
+      error: error.message, // Envia a mensagem de erro
+    });
+  }
 };
