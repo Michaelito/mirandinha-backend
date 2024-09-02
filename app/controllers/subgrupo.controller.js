c69817ac0ac5f6a058b5524fd869e2eebfd547b0
Michaelito <michaelito.regis@gmail.com>
2024-09-02 12:12:17 -0300

    ajustes grupos

--- app/controllers/subgrupo.controller.js
+++ app/controllers/subgrupo.controller.js
@@ -5,23 +5,33 @@ const sequelize = require("../config/database");
 
 // Find a single Data with an id
 exports.findOne = async (req, res) => {
+  try {
+    // Verifica se o parâmetro id é fornecido e válido, caso contrário, define como 394
+    const id = req.params.id ? req.params.id : 394;
 
-  // Verifica se o parâmetro id é fornecido e válido, caso contrário, define como 1
-  const id = req.params.id ? req.params.id : 394;
+    const results = await sequelize.query(
+      "SELECT DISTINCT g.id, g.nome nome FROM produtos p JOIN grupos g ON g.id = p.id_grupo1 WHERE p.grupo_format = " +
+        id,
+      {
+        replacements: { status: 1 },
+        type: sequelize.QueryTypes.SELECT,
+      }
+    );
 
-  const results = await sequelize.query(
-    "SELECT  DISTINCT g.id, g.nome nome FROM produtos p JOIN grupos g ON g.id = p.id_grupo1 WHERE p.grupo_format = " + id,
-    {
-      replacements: { status: 1 },
-      type: sequelize.QueryTypes.SELECT,
-    }
-  );
+    res.status(200).send({
+      status: true,
+      message: "The request has succeeded",
+      data: {
+        subgrupos: results,
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
 
-  res.status(200).send({
-    status: true,
-    message: "The request has succeeded",
-    data: {
-      subgrupos: results,
-    },
-  });
+    res.status(500).send({
+      status: false,
+      message: "An error occurred while processing your request.",
+      error: error.message,
+    });
+  }
 };
