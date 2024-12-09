bef9740c2fce082bcb5636708c8413e3f7e6ee0d
Michael Regis dos Santos <michaelito.regis@gmail.com>
2024-12-09 13:52:09 -0300

    add config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,12 +1,13 @@
 module.exports = {
 
-  //HOST: "grupomirandinha.doxotech.com.br",
-  HOST: "localhost",
-  //USER: "mirandinhadb",
-  USER: "root",
-  //PASSWORD: "YFBHSqKRmV6E",
-  PASSWORD: "",
+  //HOST: "localhost",
+  //USER: "root",
+  //PASSWORD: "",
   DB: "mirandinhadb",
+  HOST: "162.240.101.67",
+  USER: "wwport_mirandinha",
+  PASSWORD: "776336674f743d1a7f5cf692422f35be9debcebcf474d236d542cc31db036109",
+  DB: "wwport_mirandinha",
   dialect: "mysql",
   pool: {
     max: 5,
