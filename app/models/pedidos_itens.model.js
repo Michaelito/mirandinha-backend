ef5b8f28cc27de26f925c36d0bb06e0952c154a3
Michael Regis dos Santos <michaelito.regis@gmail.com>
2024-12-12 19:09:52 -0300

    ajuste pedido grade

--- app/models/pedidos_itens.model.js
+++ app/models/pedidos_itens.model.js
@@ -1,24 +1,25 @@
 module.exports = (sequelize, DataTypes) => {
-    const pedidosItens = sequelize.define("pedidos_itens", {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      produto_id: {
-        type: DataTypes.INTEGER,
-        foreignKey: true,
-      },
-      pedido_id: {
-        type: DataTypes.INTEGER,
-        foreignKey: true,
-      },
-      produto: { type: DataTypes.STRING },
-      preco: { type: DataTypes.DECIMAL(10, 4) },
-      qtde: { type: DataTypes.DECIMAL(10, 4) },
-      total: { type: DataTypes.DECIMAL(10, 4) },
-      peso: { type: DataTypes.DECIMAL(10, 4) },
-    });
-    
-    return pedidosItens;
+  const pedidosItens = sequelize.define("pedidos_itens", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    produto_id: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    pedido_id: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    produto: { type: DataTypes.STRING },
+    grade: { type: DataTypes.STRING },
+    preco: { type: DataTypes.DECIMAL(10, 4) },
+    qtde: { type: DataTypes.DECIMAL(10, 4) },
+    total: { type: DataTypes.DECIMAL(10, 4) },
+    peso: { type: DataTypes.DECIMAL(10, 4) },
+  });
+
+  return pedidosItens;
 }
