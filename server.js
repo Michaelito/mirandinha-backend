fe8f69deb76baad97c65de8c1a9b39778adf40b3
Michael Regis dos Santos <michaelito.regis@gmail.com>
2025-02-05 11:25:32 -0300

    ajustes port https

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ require("./app/routes/api/v1/payment.routes")(app);
 require("./app/routes/api/v1/deliveryArea.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8443;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
