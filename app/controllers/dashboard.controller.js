2c934693e8cecf6288ad4a1396301248ea8ac7a3
Michael Regis dos Santos <michaelito.regis@gmail.com>
2025-02-13 23:00:56 -0300

    ajuste dashboard b2b

--- app/controllers/dashboard.controller.js
+++ app/controllers/dashboard.controller.js
@@ -6,27 +6,25 @@ const { decodeTokenFromHeader } = require('../middleware/auth.js');
 
 // Retrieve all GrupoFormats from the database.
 exports.findAll = async (req, res) => {
+  const decodedToken = decodeTokenFromHeader(req);
+  const id_empresa = decodedToken.id_empresa;
 
   try {
-
-
     // 1. Extract and Validate Parameters
     const currentYear = req.query.year ? parseInt(req.query.year) : new Date().getFullYear();
-    const month = req.query.month ? parseInt(req.query.month) : null; // Use null for no month
-    const { id_empresa, id_vendedor } = req.query;
+    const month = req.query.month ? parseInt(req.query.month) : null;
+    const id_vendedor = req.query.id_vendedor;
 
     // 2. Build the WHERE Clause Dynamically
     const whereClauses = [];
-    const replacements = { currentYear }; // Start with currentYear
+    const replacements = {
+      currentYear,
+      id_empresa
+    };
 
     if (month) {
       whereClauses.push("DATE_FORMAT(createdAt, '%m') = :month");
       replacements.month = month;
-    }
-
-    if (id_empresa) {
-      whereClauses.push("id_empresa = :id_empresa");
-      replacements.id_empresa = id_empresa;
     }
 
     if (id_vendedor) {
@@ -35,31 +33,33 @@ exports.findAll = async (req, res) => {
     }
 
     whereClauses.push("DATE_FORMAT(createdAt, '%Y') = :currentYear");
+    whereClauses.push("id_empresa = :id_empresa");
 
-    const whereClause = whereClauses.length > 0 ? `WHERE ${whereClauses.join(" AND ")}` : ""; // Construct the full WHERE clause
+    const whereClause = whereClauses.length > 0 ? `WHERE ${whereClauses.join(" AND ")}` : "";
 
-    // 3. Construct the SQL Query
+    // 3. Construct the SQL Query for dashboard data
     const dashboardQuery = `
-    SELECT
-      DATE_FORMAT(createdAt, '%Y') AS year,
-      DATE_FORMAT(createdAt, '%m') AS month,
-      COUNT(id) AS count_orders,
-      SUM(total_geral) AS sum_total_orders
-    FROM
-      pedidos
-    ${whereClause}
-    GROUP BY
-      DATE_FORMAT(createdAt, '%Y'),
-      DATE_FORMAT(createdAt, '%m')
-    ORDER BY
-      DATE_FORMAT(createdAt, '%Y-%m') ASC;`;
+      SELECT
+        DATE_FORMAT(createdAt, '%Y') AS year,
+        DATE_FORMAT(createdAt, '%m') AS month,
+        COUNT(id) AS count_orders,
+        SUM(total_geral) AS sum_total_orders
+      FROM
+        pedidos
+      ${whereClause}
+      GROUP BY
+        DATE_FORMAT(createdAt, '%Y'),
+        DATE_FORMAT(createdAt, '%m')
+      ORDER BY
+        DATE_FORMAT(createdAt, '%Y-%m') ASC;
+    `;
 
-    const totalDashboard = await sequelize.query(dashboardQuery, {
+    const [dashboardData, metadataDashboard] = await sequelize.query(dashboardQuery, {
       replacements: replacements,
       type: sequelize.QueryTypes.SELECT,
     });
 
-    // 3. Construct the SQL Query
+    // 4. Construct the SQL Query for total data
     const sqlQuery = `
       SELECT 
         COUNT(id) AS total_pedidos,
@@ -69,14 +69,19 @@ exports.findAll = async (req, res) => {
       ${whereClause};
     `;
 
-    // 4. Execute the Query
-    const totalData = await sequelize.query(sqlQuery, {
+    const [totalData, metadataTotal] = await sequelize.query(sqlQuery, {
       replacements: replacements,
       type: sequelize.QueryTypes.SELECT,
     });
 
-    // 6. Extract and Send the Response
-    const { total_pedidos, total_valor } = totalData[0];
+    // 5. Extract and Send the Response
+    const {
+      total_pedidos,
+      total_valor
+    } = totalData[0] || {
+      total_pedidos: 0,
+      total_valor: 0
+    };
 
     res.status(200).send({
       status: true,
@@ -84,18 +89,19 @@ exports.findAll = async (req, res) => {
       data: {
         total_pedidos: total_pedidos || 0,
         total_valor: total_valor || 0,
-        dashboard: totalDashboard || 0
+        dashboard: dashboardData || []
       },
     });
 
   } catch (error) {
-    console.error("Error fetching data:", error); // Log the error
+    console.error("Error fetching data:", error);
     res.status(500).send({
       status: false,
       message: "The request has not succeeded",
     });
   }
 };
+
 
 
 
