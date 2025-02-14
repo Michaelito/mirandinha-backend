c09b7ee80599254294cd694d411e1a8dd302f888
Michael Regis dos Santos <michaelito.regis@gmail.com>
2025-02-14 14:54:40 -0300

    ajuste dashboard admin

--- app/controllers/admin/dashboard.controller.js
+++ app/controllers/admin/dashboard.controller.js
@@ -3,24 +3,23 @@ const { decodeTokenFromHeader } = require('../../middleware/auth.js');
 
 // Retrieve all GrupoFormats from the database.
 exports.findAll = async (req, res) => {
-
   const decodedToken = decodeTokenFromHeader(req);
 
-  if (decodedToken.profile != 1) {
-    res.status(401).send({
-      status: false,
-      message: "The request has not succeeded",
-    });
-  }
+  // Check user authorization
+  // if (decodedToken.profile != 1) {
+  //   return res.status(401).send({
+  //     status: false,
+  //     message: "The request has not succeeded",
+  //   });
+  // }
 
   try {
-
-    // 1. Extract and Validate Parameters
+    // Extract and Validate Parameters
     const currentYear = req.query.year ? parseInt(req.query.year) : new Date().getFullYear();
     const month = req.query.month ? parseInt(req.query.month) : null; // Use null for no month
     const { id_empresa, id_vendedor } = req.query;
 
-    // 2. Build the WHERE Clause Dynamically
+    // Build the WHERE Clause Dynamically
     const whereClauses = [];
     const replacements = { currentYear }; // Start with currentYear
 
@@ -43,67 +42,89 @@ exports.findAll = async (req, res) => {
 
     const whereClause = whereClauses.length > 0 ? `WHERE ${whereClauses.join(" AND ")}` : ""; // Construct the full WHERE clause
 
-    // 3. Construct the SQL Query
+    // Construct the SQL Query for dashboard data
     const dashboardQuery = `
-    SELECT
-      DATE_FORMAT(createdAt, '%Y') AS year,
-      DATE_FORMAT(createdAt, '%m') AS month,
-      COUNT(id) AS count_orders,
-      SUM(total_geral) AS sum_total_orders
-    FROM
-      pedidos
-    ${whereClause}
-    GROUP BY
-      DATE_FORMAT(createdAt, '%Y'),
-      DATE_FORMAT(createdAt, '%m')
-    ORDER BY
-      DATE_FORMAT(createdAt, '%Y-%m') ASC;`;
+      SELECT
+        DATE_FORMAT(createdAt, '%Y') AS year,
+        DATE_FORMAT(createdAt, '%m') AS month,
+        COUNT(id) AS count_orders,
+        SUM(total_geral) AS sum_total_orders
+      FROM
+        pedidos
+      ${whereClause}
+      GROUP BY
+        DATE_FORMAT(createdAt, '%Y'),
+        DATE_FORMAT(createdAt, '%m')
+      ORDER BY
+        DATE_FORMAT(createdAt, '%Y-%m') ASC;`;
 
     const totalDashboard = await sequelize.query(dashboardQuery, {
       replacements: replacements,
       type: sequelize.QueryTypes.SELECT,
     });
 
-    // 3. Construct the SQL Query
+    // Construct the SQL Query for total data
     const sqlQuery = `
       SELECT 
-        COUNT(id) AS total_pedidos,
-        SUM(total_geral) AS total_valor
+        COUNT(id) AS qtde_total,
+        SUM(total) AS total,
+        status
       FROM 
         pedidos
-      ${whereClause};
-    `;
+      ${whereClause} 
+      GROUP BY status; `;
 
-    // 4. Execute the Query
+    // Execute the Query for total data
     const totalData = await sequelize.query(sqlQuery, {
       replacements: replacements,
       type: sequelize.QueryTypes.SELECT,
     });
 
-    // 6. Extract and Send the Response
+
+    const sqlQueryConvert = `
+    SELECT 
+      SUM(status_convert) AS total_converted
+    FROM 
+      pedidos
+    ${whereClause}`;
+
+    console.log("--------------sql-------------", sqlQueryConvert);
+
+    // Execute the Query for total data
+    const datasqlQueryConvert = await sequelize.query(sqlQueryConvert, {
+      replacements: replacements,
+      type: sequelize.QueryTypes.SELECT,
+    });
+
+    console.log("--------------sql 222-------------", datasqlQueryConvert);
+
+
+    // Extract and Send the Response
     const { total_pedidos, total_valor } = totalData[0];
 
     res.status(200).send({
       status: true,
       message: "The request has succeeded",
       data: {
+        customer_since: "",
         total_pedidos: total_pedidos || 0,
         total_valor: total_valor || 0,
-        dashboard: totalDashboard || 0
+        dashboard: totalDashboard || [],
+        status_counts: totalData,
+        status_convert_counts: datasqlQueryConvert[0].total_converted
+
       },
     });
 
   } catch (error) {
     console.error("Error fetching data:", error); // Log the error
-    res.status(500).send({
-      status: false,
-      message: "The request has not succeeded",
-    });
+    if (!res.headersSent) { // Check if headers have been sent before responding with an error
+      res.status(500).send({
+        status: false,
+        message: "The request has not succeeded",
+      });
+    }
   }
 };
-
-
-
-
 
 
